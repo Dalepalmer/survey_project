feb45d1647381092421326f9a675c3ee48eb7148
dalepalmer & phil orchard <student@epicodus.com>
2015-01-28 15:14:55 -0800

    full site functionality

--- app.rb
+++ app.rb
@@ -11,19 +11,22 @@ get('/') do
   erb(:index)
 end
 
+get("/question") do
+  @question = Question.all()
+  erb(:question)
+end
+
 post("/questions") do
   question_name = params.fetch("question_name")
   survey_id = params.fetch("survey_id").to_i()
-  question = Question.new({:question_name => question_name, :survey_id => survey_id})
-  question.save()
+  question = Question.create({:question_name => question_name, :survey_id => survey_id})
   @survey = Survey.find(survey_id)
   erb(:survey)
 end
 
 post('/surveys') do
   survey_name = params.fetch("survey_name")
-  survey = Survey.new({:survey_name => survey_name, :id => nil})
-  survey.save()
+  @survey = Survey.create({:survey_name => name})
   @surveys = Survey.all()
   erb(:index)
 end
@@ -34,8 +37,8 @@ get("/surveys/:id") do
 end
 
 get("/surveys/:id/edit") do
-  @survey = Survey.find(params.fetch("id").to_id())
-  erb(:survey)
+  @survey = Survey.find(params.fetch("id").to_i())
+  erb(:survey_edit)
 end
 
 patch("/surveys/:id") do
@@ -50,4 +53,24 @@ delete("/surveys/:id") do
   @survey.delete()
   @surveys = Survey.all()
   erb(:index)
+end
+
+get("/questions/:id/edit") do
+  @question = Question.find(params.fetch("id").to_i())
+  erb(:question_edit)
+end
+
+patch("/questions/:id") do
+  question_name = params.fetch("question_name")
+  @question = Question.find(params.fetch("id").to_i())
+  @question.update({:question_name => question_name})
+  @survey = @question.survey()
+  erb(:survey)
+end
+
+delete("/questions/:id") do
+  @question = Question.find(params.fetch("id").to_i())
+  @question.delete()
+  @questions = Question.all()
+  erb(:survey)
 end
--- lib/survey.rb
+++ lib/survey.rb
@@ -1,6 +1,6 @@
 class Survey < ActiveRecord::Base
   has_many(:questions)
-
+  validates(:name, {:presence => true, :length => {:maximum => 100}})
 
   define_method(:add_question) do |question|
     question.update({:survey_id => self.id().to_i()})
