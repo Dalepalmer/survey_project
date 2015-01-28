69b369a99c47183dd1df2b44b029e7916b74f084
dalepalmer & phil orchard <student@epicodus.com>
2015-01-28 12:22:46 -0800

    run on sinatra

--- app.rb
+++ app.rb
@@ -11,10 +11,43 @@ get('/') do
   erb(:index)
 end
 
+post("/questions") do
+  question_name = params.fetch("question_name")
+  survey_id = params.fetch("survey_id").to_i()
+  question = Question.new({:question_name => question_name, :survey_id => survey_id})
+  question.save()
+  @survey = Survey.find(survey_id)
+  erb(:survey)
+end
+
 post('/surveys') do
   survey_name = params.fetch("survey_name")
   survey = Survey.new({:survey_name => survey_name, :id => nil})
   survey.save()
+  @surveys = Survey.all()
+  erb(:index)
+end
+
+get("/surveys/:id") do
+  @survey = Survey.find(params.fetch("id").to_i())
+  erb(:survey)
+end
+
+get("/surveys/:id/edit") do
+  @survey = Survey.find(params.fetch("id").to_id())
+  erb(:survey)
+end
+
+patch("/surveys/:id") do
+  survey_name = params.fetch("survey_name")
+  @survey = Survey.find(params.fetch("id").to_i())
+  @survey.update({:survey_name => survey_name})
+  erb(:survey)
+end
+
+delete("/surveys/:id") do
+  @survey = Survey.find(params.fetch("id").to_i())
+  @survey.delete()
   @surveys = Survey.all()
   erb(:index)
 end
